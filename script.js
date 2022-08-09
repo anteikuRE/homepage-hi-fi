d0fe3b6b9dd865bf79f3b0e1f3b9b0dbc1a0abc6
anteikuRE <tankerbit@gmail.com>
2022-08-09 04:33:43 +0300

    second section rows + js for them

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const login = document.getElementById('login');
 const modal = document.querySelector('.modal');
 const hidden = document.querySelector('.hidden');
 const overlay = document.querySelector('.overlay');
-const closeModal = document.querySelector('.btn--close-modal');
+const closeModal = document.querySelector('.btn__close_modal');
+const arrow = document.querySelectorAll('.second-screen__row__arrow');
+const learnLabe = document.querySelectorAll('.second-screen__row__arrow__learn')
+
+// 
 
 login.addEventListener('click', function () {
     modal.classList.remove('hidden')
@@ -16,3 +20,31 @@ closeModal.addEventListener('click', function () {
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
 })
+
+// 
+// console.log(learnLabe.innerHTML);
+
+// let as = true;
+
+arrow.forEach(element => {
+    element.addEventListener('click', function (event) {
+
+        const d = event.target.parentNode.firstChild.nextSibling;
+        console.log(d.classList.contains('active'));
+        // console.log(d.style.opacity === '0')
+        if (d.classList.contains('active') === false) {
+
+            d.style.opacity = '100'
+
+            event.target.parentNode.style.boxShadow = 'inset -298px 0 0 0 var(--aqua)'
+            event.target.style.transform = 'rotateZ(45deg)';
+            d.classList.add('active')
+        }
+        else if (d.classList.contains('active') === true) {
+            d.style.opacity = '0'
+            event.target.parentNode.style.boxShadow = 'inset -60px 0 0 0 var(--aqua)'
+            event.target.style.transform = 'rotateZ(0deg)';
+            d.classList.remove('active')
+        }
+    })
+});
